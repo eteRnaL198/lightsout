90ec0d205d4f00993bb2318178789eda4f137653
eternal198 <ikuya313198@gmail.com>
2021-04-16 18:16:36 +0900

    completed

--- Script.js
+++ Script.js
@@ -1,23 +1,32 @@
-//numberという配列を持たせてその中の要素が全部0(ライツアウトでいう消灯)になったら終了
-let number = number(16);
-let n;
-let choice;
+const arr = [];
+for(let i=0; i<16; i++) {
+    const wrapper = document.getElementById('js_box_wrapper');  
+    const box = document.createElement('div');
 
-for (let n = 0; n <16; n++){
-    number(n)=1;
-}
+    box.setAttribute('id', `box${i}`);
+    (i % 4 === 0) ? box.setAttribute('class', 'hidden'): box.setAttribute('class', 'box box-off');
+    arr.push(box);
+    box.addEventListener('click', () => {
+        (arr[i-4] ? arr[i-4].classList.toggle('box-off') : null );
+        (arr[i-1] ? arr[i-1].classList.toggle('box-off') : null );
+        (arr[i] ? arr[i].classList.toggle('box-off') : null );
+        (arr[i+1] ? arr[i+1].classList.toggle('box-off') : null );
+        (arr[i+4] ? arr[i+4].classList.toggle('box-off') : null );
 
-while (1){
+        let flag = 0;
+        for(let j=0; j<16; j++) {
+            if(j % 4 !== 0 && arr[j].classList.contains('box-off')) {
+                flag++;
+            }
+        }
+        console.log(flag);
+        if(flag == 1) {
+            const message = document.createElement('p');
+            message.setAttribute('class', 'message');
+            message.innerHTML = 'Completed';
+            wrapper.parentNode.insertBefore(message, wrapper);
+        }
+    });
     
-    if(number(0)===0 &&
-       number(1)===0 &&
-       number(2)===0 &&
-       number(5)===0 && 
-       number(6)===0 && 
-       number(7)===0 && 
-       number(9)===0 && 
-       number(10)===0 && 
-       number(11)===0){
-       break; 
-    }
+    wrapper.appendChild(box);
 }
